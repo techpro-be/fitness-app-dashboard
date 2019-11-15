5e88903c6f878e55692abcc9ea0187e86d30804b
mario-neb <50882870+mario-neb@users.noreply.github.com>
2019-11-15 18:03:19 +0100

    fixes on template

--- src/app/modules/user/components/display-user/display-user.component.ts
+++ src/app/modules/user/components/display-user/display-user.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as jspdf from 'jspdf';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 import { UserService } from '../../services/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { Input } from '@angular/core';
+
 @Component({
   selector: 'app-display-user',
   templateUrl: './display-user.component.html',
@@ -12,6 +13,7 @@ import { Input } from '@angular/core';
 export class DisplayUserComponent implements OnInit {
 
   @Input() resumes;
+  @ViewChild('content', {static: false}) content: ElementRef;
   resume: any;
   constructor(
     private route: ActivatedRoute,
@@ -27,25 +29,35 @@ export class DisplayUserComponent implements OnInit {
     });
   }
 
-  public captureScreen() {
-    const data = document.getElementById('contentToConvert');
-    html2canvas(data).then(canvas => {
-      // Few necessary setting options
-      const imgWidth = 208;
-      const imgHeight = canvas.height * imgWidth / canvas.width;
+  public downloadCv() {
+    // const data = document.getElementById('contentToConvert');
+    // html2canvas(data).then(canvas => {
+    //   // Few necessary setting options
+    //   const imgWidth = 208;
+    //   const imgHeight = canvas.height * imgWidth / canvas.width;
 
-      const contentDataURL = canvas.toDataURL('image/png');
-      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-      const position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-      pdf.save('MYPdf.pdf'); // Generated PDF
+    //   const contentDataURL = canvas.toDataURL('image/png');
+    //   const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+    //   const position = 0;
+    //   pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+    //   pdf.save('MYPdf.pdf'); // Generated PDF
+    // });
+
+    const doc = new jsPDF();
+    const specialElementHandlers = {
+      '#editor'(element, renderer) {
+        return true;
+      }
+    };
+
+    const content = this.content.nativeElement;
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      width : 50,
+      elementHandlers : specialElementHandlers
     });
+
+    setTimeout(function() {
+      doc.save('Cv.pdf');
+  }, 0);
   }
-
-  // captureScreenWord() {
-  //   const converted = htmlDocx.asBlob(contentToConvert);
-  //   saveAs(converted, 'test.docx');
-  // }
-
-
 }
