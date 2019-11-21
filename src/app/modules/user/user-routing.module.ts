72237f4679e3bf830c9858f72cbc3fb0065ef3ad
mario-neb <50882870+mario-neb@users.noreply.github.com>
2019-11-21 10:28:02 +0100

    minor fix on layout

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -5,22 +5,21 @@ import { DisplayUserComponent } from './components/display-user/display-user.com
 import { LayoutComponent } from 'src/app/shared/components/layout/layout.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
+  // {
+  //   path: '',
+  //   component: LayoutComponent,
+  //   children: [
       {
         path: 'user-list',
         component: UserListComponent,
         pathMatch: 'full'
       },
-
       {path: 'display-user/:id',
       pathMatch: 'full',
       component: DisplayUserComponent
       }
-    ]
-  }
+  //   ]
+  // }
 ];
 
 @NgModule({
